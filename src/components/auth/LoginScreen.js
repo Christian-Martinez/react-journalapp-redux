56fdc0ef92199dd0c9a945bc8bac9d8e2b276551
Christian-Martinez <christ96mart@gmail.com>
2021-03-11 12:33:45 -0400

    form de registro y validaciones

--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 
 import { useForm } from '../../hooks/useForm';
 import { startGoogleLogin, startLoginEmailPassword } from '../../actions/auth';
@@ -42,7 +42,7 @@ export const LoginScreen = () => {
           placeholder='Password'
           name='password'
           className='auth__input'
-          value={email}
+          value={password}
           onChange={handleInputChange}
         />
         <button type='submit' className='btn btn-primary btn-block'>
--- src/components/auth/RegisterScreen.js
+++ src/components/auth/RegisterScreen.js
@@ -1,61 +1,94 @@
 import React from 'react';
+import validator from 'validator';
 import { Link } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+
+import { useForm } from '../../hooks/useForm';
+import { startLoginEmailPassword } from '../../actions/auth';
 
 export const RegisterScreen = () => {
-    return (
-        <>
-            <h3 className="auth__title">Register</h3>
+  // const dispatch = useDispatch();
 
-            <form>
+  const [formValues, handleInputChange] = useForm({
+    name: 'Hernando',
+    email: 'nando@gmail.com',
+    password: '123456',
+    password2: '123456',
+  });
 
-                <input 
-                    type="text"
-                    placeholder="Name"
-                    name="name"
-                    className="auth__input"
-                    autoComplete="off"
-                />
+  const { name, email, password, password2 } = formValues;
 
-                <input 
-                    type="text"
-                    placeholder="Email"
-                    name="email"
-                    className="auth__input"
-                    autoComplete="off"
-                />
+  const handleRegister = (e) => {
+    e.preventDefault();
+    console.log(name);
+    if (isFormValid()) {
+      // dispatch( startRegisterWithEmailPasswordName(email, password, name) );
+    }
+  };
 
-                <input 
-                    type="password"
-                    placeholder="Password"
-                    name="password"
-                    className="auth__input"
-                />
+  const isFormValid = () => {
+    if (name.trim().length === 0) {
+      // dispatch(setError('Name is required'));
+      console.log('Name is required');
+      return false;
+    } else if (!validator.isEmail(email)) {
+      // dispatch(setError('Email is not valid'));
+      console.log('Email is required');
+      return false;
+    } else if (password !== password2 || password.length < 5) {
+      // dispatch(setError('Password should be at least 6 characters and match each other'));
+      console.log('password');
+      return false;
+    }
+    // dispatch(removeError());
+    return true;
+  };
 
-                <input 
-                    type="password"
-                    placeholder="Confirm password"
-                    name="password2"
-                    className="auth__input"
-                />
-
-
-                <button
-                    type="submit"
-                    className="btn btn-primary btn-block mb-5"
-                >
-                    Register
-                </button>
-
-               
-
-                <Link 
-                    to="/auth/login"
-                    className="link"
-                >
-                    Already registered?
-                </Link>
-
-            </form>
-        </>
-    )
-}
+  return (
+    <>
+      <h3 className='auth__title'>Register</h3>
+      <form onSubmit={handleRegister}>
+        <input
+          type='text'
+          placeholder='Name'
+          name='name'
+          className='auth__input'
+          autoComplete='off'
+          value={name}
+          onChange={handleInputChange}
+        />
+        <input
+          type='text'
+          placeholder='Email'
+          name='email'
+          className='auth__input'
+          autoComplete='off'
+          value={email}
+          onChange={handleInputChange}
+        />
+        <input
+          type='password'
+          placeholder='Password'
+          name='password'
+          className='auth__input'
+          value={password}
+          onChange={handleInputChange}
+        />
+        <input
+          type='password'
+          placeholder='Confirm password'
+          name='password2'
+          className='auth__input'
+          value={password2}
+          onChange={handleInputChange}
+        />
+        <button type='submit' className='btn btn-primary btn-block mb-5'>
+          Register
+        </button>
+        <Link to='/auth/login' className='link'>
+          Already registered?
+        </Link>
+      </form>
+    </>
+  );
+};
